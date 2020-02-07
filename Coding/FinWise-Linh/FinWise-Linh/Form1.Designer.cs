ea67aeea09784ef8fa9668ad14258c0901228e17
linhqtruong <admin@linhtruong.com>
2020-02-06 22:15:12 -0700

    Add files via upload

---
+++ Coding/FinWise-Linh/FinWise-Linh/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace FinWise_Linh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.tbOpenFile = new System.Windows.Forms.TextBox();
+            this.txbSaveFileName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.GridView = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(328, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FinWise Bank";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Open File:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(620, 43);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Open File";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // tbOpenFile
+            // 
+            this.tbOpenFile.Location = new System.Drawing.Point(73, 46);
+            this.tbOpenFile.Name = "tbOpenFile";
+            this.tbOpenFile.Size = new System.Drawing.Size(541, 20);
+            this.tbOpenFile.TabIndex = 3;
+            // 
+            // txbSaveFileName
+            // 
+            this.txbSaveFileName.Location = new System.Drawing.Point(73, 72);
+            this.txbSaveFileName.Name = "txbSaveFileName";
+            this.txbSaveFileName.Size = new System.Drawing.Size(541, 20);
+            this.txbSaveFileName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Save File:";
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.Location = new System.Drawing.Point(15, 132);
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(773, 191);
+            this.rtbInput.TabIndex = 7;
+            this.rtbInput.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Input:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "GridView:";
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(9, 699);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(773, 152);
+            this.rtbOutput.TabIndex = 10;
+            this.rtbOutput.Text = "";
+            // 
+            // GridView
+            // 
+            this.GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView.Location = new System.Drawing.Point(12, 342);
+            this.GridView.Name = "GridView";
+            this.GridView.Size = new System.Drawing.Size(770, 328);
+            this.GridView.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 683);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Output:";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStatus.Location = new System.Drawing.Point(620, 75);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(166, 13);
+            this.lbStatus.TabIndex = 13;
+            this.lbStatus.Text = "STATUS: Please Select File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 856);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.GridView);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rtbInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbSaveFileName);
+            this.Controls.Add(this.tbOpenFile);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Linh FinWise Bank";            
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox tbOpenFile;
+        private System.Windows.Forms.TextBox txbSaveFileName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.DataGridView GridView;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbStatus;
+    }
+}
+
---
+++ Coding/FinWise-Linh/FinWise-Linh/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FinWise_Linh
+{
+    public partial class Form1 : Form
+    {
+
+        string[] lines = null;
+        string json = null;
+        string outputFileName = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = false;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = Path.GetFileName(openFileDialog1.FileName);
+                string inputPath = openFileDialog1.FileName;
+                tbOpenFile.Text= inputPath;
+                readFile(inputPath);
+                coverToJson();
+                //Display in GridView
+                GridView.DataSource = JsonConvert.DeserializeObject<DataTable>(json);
+                _ = GridView.DataBindings;
+                //save file
+                saveFile();
+                lbStatus.Text = "Done: File Saved";
+                lbStatus.ForeColor = Color.Green;
+            }
+        }
+
+        private void readFile(String inputPath)
+        {
+            try
+                { lines = System.IO.File.ReadAllLines(inputPath);}
+            catch (FileNotFoundException e)
+                {Console.WriteLine("File not found");}
+            foreach(string line in lines)
+                rtbInput.Text += line + "\n";                                   
+        }
+
+        // Convert Data to JSON Data
+        private void coverToJson()
+        {              
+            if (lines != null)
+                {
+                    json = "[";
+                    for (int i = 1; i < lines.Length - 2; i++)
+                        json += "{" + "'Name': '" + lines[i].Substring(9, 20).Trim() + "','Account': " + lines[i].Substring(32, 11).Trim() + ",'Amount': '" + lines[i].Substring(45, 10) + "'},";
+               
+                    json = json.Remove(json.Length - 1);
+                    json += "]";
+                    json = json.Replace("'", "\"");
+                }
+            rtbOutput.Text = json;
+        }
+
+        private void saveFile()
+        {
+            //GENERATE JSON FILENAME in MMDDYYYY-HHMM format
+            outputFileName = @"C:\ATF-Output\ATF" + string.Format(DateTime.Now.ToString("MMddyyyy-hhmm")) + ".etfx";
+            txbSaveFileName.Text = outputFileName;
+            //WRITE JSON DATA to File
+            System.IO.File.WriteAllText(outputFileName, json);
+        }
+    }
+}
